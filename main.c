8663c3845f031fb13d9f86f615d1d9cced0786ab
quaxton <jyang772@gmail.com>
2018-01-05 03:13:50 -0500

    clean main

--- main.c
+++ main.c
@@ -40,26 +40,18 @@ int main() {
 	Packet data;
 	int goodReply = 1;
 	int rd = 0;
+
+	/* Handshake */
 	radA->init_connection(radA);
-	while(radA->getTemp(radA) < 0);
+	
+	/* Repeatedly request temperature from Node B */
+	while(1) {
+		while(radA->getTemp(radA) < 0);
+		sleep(1);
+	}
 	return 0;
 
-	while(1) {
-		radA->getTemp(radA);
-		//radA->init_connection(radA);
-		pret = poll(fds, 2, timeout);
-		if(pret == 0) {
-			printf("ret %d\n",pret);
-			printf("\ttimeout\n");
-		}
-		else {
-			int rd = safe_read(fd, &data, sizeof(Packet)); /* Check for errors */
-			if(rd > 0) {
-				printf("payload: %s\n",data.payload);
-			}
-		}	
-		sleep(1); /* Allow time for node to transmit reply */
-	}
+
 
 	//radA->sendCmd("CMD");
 	//radA->getTemp();
